e124624c4f0715d6b2da11e9db8243b7287326e1
Ryan Omasta <ryand@emeraldsys.xyz>
2022-09-05 01:49:36 -0600

    Add boilerplate for effect

---
+++ ChaosVDotNet/Effects/db/Ped/AttackPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChaosVDotNet.Effects.db
+{
+    internal class AttackPlayer : Effect
+    {
+        public AttackPlayer() : base("All Peds Attack Player", EffectType.Ped, true, false)
+        {
+
+        }
+    }
+}
